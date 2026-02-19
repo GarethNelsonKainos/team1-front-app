f5e941e287793b05bf564dfa4bcc3c5b859e9b6d
MatthewDobsonKainos <matthew.dobson@kainos.com>
2026-02-19 12:00:41 +0000

    Resolved some of the errors in JobRoleController.ts.

--- src/controllers/JobRoleController.ts
+++ src/controllers/JobRoleController.ts
@@ -22,8 +22,6 @@ const upload = multer({
   },
 });
 
-const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:3001';
-
 export default function JobRoleController(
   app: Application,
   jobRoleService: JobRoleService,
