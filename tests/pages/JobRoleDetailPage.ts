f26a665d3b81375f0ed10d4d3ad249d7dfaf8b58
jos-io <ethan.campbell@kainos.com>
2026-02-25 16:20:09 +0000

    new test for job role details page navigation without using ai

--- tests/pages/JobRoleDetailPage.ts
+++ tests/pages/JobRoleDetailPage.ts
@@ -3,13 +3,23 @@ import { BasePage } from './BasePage';
 
 export class JobRoleDetailPage extends BasePage {
   private readonly heading: Locator;
+  private readonly backToJobRolesButton: Locator;
 
   constructor(page: Page) {
     super(page);
     this.heading = page.locator('h1');
+    this.backToJobRolesButton = page.locator('a:has-text("Back to Job Roles")');
   }
 
   async isHeadingVisible(): Promise<boolean> {
     return this.heading.isVisible();
+  }
+
+  async clickBackToJobRoles(): Promise<void> {
+    await this.backToJobRolesButton.click();
+  }
+
+  async getUrl(): Promise<string> {
+    return this.page.url();
   }
 }
